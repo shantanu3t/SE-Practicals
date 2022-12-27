635d787296f4bfbe25244428d9cba30ae8eca701
Shantanu Avinash Todkar <95854064+shantanu3t@users.noreply.github.com>
2022-12-28 00:24:48 +0530

    Update d5.py

--- d5.py
+++ d5.py
@@ -1,47 +1,58 @@
-def selection_sort(lis, size):
-    for i in range(size):
-        mainIndex=i
-        for currIndex in range(i+1,size):
-            if(lis[mainIndex]>lis[currIndex]):
-                mainIndex=currIndex
-        (lis[i],lis[mainIndex])=(lis[mainIndex], lis[i])
-
-def bubble_sort(lis,size):
-    for i in range(size-1):
-        for j in range(size-i-1):
-            if(lis[j]>lis[j+1]):
-                (lis[j],lis[j+1])=(lis[j+1],lis[j])
-
-if __name__=='__main__':
-    print('''
-            *************Menu***********
-    
-            1. Enter list
-            2. Print list
-            3. Selection sort
-            4. Bubble sort
-            5. Break
-            ''')
-    option =0
-    lis=[7,35,1345,134,5,45,5634,352,52,5215,1,2,3,4,5,6,7,8]
-
-    while(option!=4):
-        option=int(input("Enter the option: "))
+def selection_sort(marks):
+    for i in range(len(marks)):
+        min_idx = i
+        for j in range(i+1, len(marks)):
+            if marks[min_idx] > marks[j]:
+                min_idx = j
+        marks[i], marks[min_idx] = marks[min_idx], marks[i]
+    print("Marks after performing selection sort:",marks)
+    for i in range(len(marks)):
+        print(marks[i])
 
 
-        if(option==1):
-            leng=int(input("Enter the length of string: "))
-            for i in range(leng):
-                l=int(input(f"Enter the {i}th element :"))
-                lis.append(l)
+def bubble_sort(marks):
+    n = len(marks)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if marks[j] > marks[j+1]:
+                marks[j], marks[j+1] = marks[j+1], marks[j]
+    print("Marks after performing bubble sort:")
+    for i in range(len(marks)):
+        print(marks[i])
 
-        elif(option==2):
-            for items in lis:
-                print(items,end=" ")
-            print()
 
-        elif(option==3):
-            selection_sort(lis, len(lis))
-            
-        elif(option==4):
-            bubble_sort(lis, len(lis))
+def top_five_marks(marks):
+    for i in marks:
+        #highest = max(marks)
+        #last = marks[-1]
+        m = (sorted(marks, reverse=True))
+        print(m[0:5])
+        break
+
+
+marks = []
+n = int(input("Enter number of students whoes marks are to be displayed:"))
+print("Enter marks for", n, "students(press ENTER after every students marks):")
+for i in range(0, n):
+    ele = int(input())
+    marks.append(ele)
+print("The marks of", n, "students are:")
+print(marks)
+print("Menu:\n1.Selection sort\n2.Bubble sort\n3.Exit")
+while (1):
+    s = int(input("\nChoose your option:"))
+    if (s == 1):
+        print("\nSelection sort:")
+        selection_sort(marks)
+        print("\nTop five marks:")
+        top_five_marks(marks)
+    elif (s == 2):
+        print("\nBubble sort:")
+        bubble_sort(marks)
+        print("\nTop five marks:")
+        top_five_marks(marks)
+    elif (s == 3):
+        print("Exit")
+        break
+    else:
+        print("Invalid Option!")
