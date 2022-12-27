908c65a5d6ef5e521f04396837d7477756e9249d
Shantanu Avinash Todkar <95854064+shantanu3t@users.noreply.github.com>
2022-12-27 23:10:27 +0530

    Update d2.py

--- d2.py
+++ d2.py
@@ -1,93 +1,59 @@
-def wordWithLonLen(str1):
-    list1=str1.split(" ")
-    str2=""
-    for i in range(len(list1)):
-        if(len(list1[i])>len(str2)):
-            str2=list1[i]
-    return str2
-
-def freqofoccur(str1):
-    count=0
-    char=input("Enter the character: ")
-    for i in range(len(str1)):
-        if(str1[i]==char):
-            count=count+1
-    return count
-
-def check_palindrome(str1):
-    i=0
-    j=len(str1)-1
-    while(i<=j):
-        if(str1[i]!=str1[j]):
-            return 0
-        i+=1 
-        j-=1
-    return 1
-
-def firapp(str1):
-    str2=input("Enter the substring: ")
-    return str1.find(str2)
-
-def wordocc(str1):
-    count=0
-    dict={}
-    list1=str1.split(" ")
-    for i in range(len(list1)):
-        for j in range(len(list1)):
-            if(list1[i]==list1[j]):
-                count+=1
-        dict[list1[i]]=count
-    return dict
-
-
-if __name__=='__main__':
-    str=""
-    print('''
-            *************Menu***********
-    
-            1. Enter String
-            2. Display word with the longest length
-            3. Determines the frequency of occurrence of particular character in the string
-            4. Check whether given string is palindrome or not
-            5. Display index of first appearance of the substring
-            6. Count the occurrences of each word in a given string
-            7. Break
-            ''')
-    option =0
-    str1=""
-    while(option!=7):
-        option=int(input("Enter the option: "))
-        if(option==1):
-            str1=input("Enter the string: ")
-    
-        elif(option==2):
-            if(len(str1)==0):
-                print("First enter the string")
-            else:
-                print(wordWithLonLen(str1)) 
-
-        elif(option==3):
-            if(len(str1)==0):
-                print("First enter the string")
-            else:
-                print(freqofoccur(str1)) 
-                
-        elif(option==4):
-            if(len(str1)==0):
-                print("First enter the string")
-            else:
-                if(check_palindrome(str1)):
-                    print("Given string is palindrome")
-                else:
-                    print("Given string is not palindrome")
-        elif(option==5):
-            if(len(str1)==0):
-                print("First enter the string")
-            else:
-                print(firapp(str1)) 
-        elif(option==6):
-            if(len(str1)==0):
-                print("First enter the string")
-            else:
-                print(wordocc(str1)) 
-
+str1 = input("Enter the main string :")
+def s1():
+    long = []
+    s = str1.split()
+    for i in s:
+        if(len(i) > len(long)):
+            long = i
+    print("Word with longest length is operations having length:", long)
+def s2():
+    d = input("Enter the character :")
+    e = str1.count(d)
+    print("Frequency of occurrence of character(s) in string (", d, ") is", e)
+def s3():
+    a = str1[::-1]
+    if(a == str1):
+        print(a, " string is an palindrome string")
+    else:
+        print(a, " string is not a palindrome string")
+def s4():
+    c = input("Enter the sub string to check : ")
+    b = str1.find(c)
+    #print("Main String :", str1);
+    #print("Substring String :", c);
+    print("Substring string found at index", b)
+def s5():
+    words = str1.split()
+    words.sort()
+    print(words)
+    occ=1
+    prev = words[0]
+    for i in range(1, len(words)):
+        if(words[i] != prev):
+            print(prev, ":", occ)
+            prev = words[i]
+            occ = 1
+        else:
+            occ += 1
+            print(prev, ":", occ)
+    print(words[len(words)-1], ":", occ)
+while(True):
+    print("1 : Display word with longest length")
+    print("2 : Determine the frequency of occurrence of particular character in the string")
+    print("3 : Check whether given string is palindrome or not")
+    print("4 : Display index of first appearance of the substring")
+    print("5 : Count the occurrences of each word in a given string")
+    print("6 : Exit")
+    func = int(input("Enter your choice: "))
+    if(func==1):
+        p=s1()
+    elif(func==2):
+        q=s2()
+    elif(func==3):
+        r=s3()
+    elif(func==4):
+        s=s4()
+    elif(func==5):
+        t=s5()
+    else:
+        break
